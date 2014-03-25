0ada2fbc930e1e2b2597641b841cb245d99a6d13
Pedro Alonso Ferrer <pedro.paf@gmail.com>
2014-03-25 21:33:45 +0000

    Initial check-in

---
+++ StripeSaas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StripeSaas.Startup))]
+namespace StripeSaas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
