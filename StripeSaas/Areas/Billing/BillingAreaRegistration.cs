54d87be8953c22e31052f46b948524b9765cb5f2
Pedro Alonso <pedro@pedroalonso.net>
2014-03-28 12:27:15 +0000

    Added billing area and index view.

---
+++ StripeSaas/Areas/Billing/BillingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace StripeSaas.Areas.Billing
+{
+    public class BillingAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Billing";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Billing_default",
+                "Billing/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ StripeSaas/Areas/Billing/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StripeSaas.Areas.Billing.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Billing/Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
