7d8a35b0125ad7c98ee195e144e28b612931535c
Pedro Alonso <pedro@pedroalonso.net>
2014-04-28 17:45:30 +0100

    Removed IOC

--- SaasEcom.Web/Global.asax.cs
+++ SaasEcom.Web/Global.asax.cs
@@ -1,6 +1,4 @@
-﻿using SimpleInjector;
-using SimpleInjector.Integration.Web.Mvc;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -14,26 +12,6 @@ namespace SaasEcom.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            RegisterSimpleInjector();
-        }
-
-        private void RegisterSimpleInjector()
-        {
-            // Create the container as usual.
-            var container = new Container();
-
-            // Register your types, for instance:
-            // container.Register<IUserRepository, SqlUserRepository>();
-
-            // This is an extension method from the integration package.
-            // container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
-
-            // This is an extension method from the integration package as well.
-            // container.RegisterMvcIntegratedFilterProvider();
-
-            container.Verify();
-
-            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
         }
     }
 }
