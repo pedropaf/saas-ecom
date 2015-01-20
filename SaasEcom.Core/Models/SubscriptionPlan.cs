ac912da2b67d4e755680e35451f8ede25ba94dbf
pedropaf <pedro@pedroalonso.net>
2015-01-20 17:56:01 -0500

    Moved strings to resources.

--- SaasEcom.Core/Models/SubscriptionPlan.cs
+++ SaasEcom.Core/Models/SubscriptionPlan.cs
@@ -154,28 +154,31 @@ namespace SaasEcom.Core.Models
             /// <summary>
             /// Monthly
             /// </summary>
+            [Display(ResourceType = typeof (Resources.SaasEcom), Name = "SubscriptionInterval_Monthly_Monthly")]
             Monthly = 1,
 
             /// <summary>
             /// Yearly
             /// </summary>
+            [Display(ResourceType = typeof (Resources.SaasEcom), Name = "SubscriptionInterval_Yearly_Yearly")]
             Yearly = 2,
 
             /// <summary>
             /// Weekly
             /// </summary>
+            [Display(ResourceType = typeof (Resources.SaasEcom), Name = "SubscriptionInterval_Weekly_Weekly")]
             Weekly = 3,
 
             /// <summary>
             /// Every 6 months
             /// </summary>
-            [Display(Name="Every 6 months")]
+            [Display(ResourceType = typeof (Resources.SaasEcom), Name = "SubscriptionInterval_EverySixMonths_Every_6_months")]
             EverySixMonths = 4,
 
             /// <summary>
             /// Every 3 months
             /// </summary>
-            [Display(Name="Every 3 months")]
+            [Display(ResourceType = typeof (Resources.SaasEcom), Name = "SubscriptionInterval_EveryThreeMonths_Every_3_months")]
             EveryThreeMonths = 5
         }
     }
