c61afaeb19b1964eebac62faddde85c5bb252a21
pedropaf <pedro@pedroalonso.net>
2015-01-29 22:30:56 -0500

    Moved stripe errors to js file so they can be customized / translated.

--- SaasEcom.FrontEnd/content/Scripts/saasecom.card.form.js
+++ SaasEcom.FrontEnd/content/Scripts/saasecom.card.form.js
@@ -3,9 +3,24 @@
 var stripeResponseHandler = function (status, response) {
     var $form = $('#card-form');
 
-    if (response.error) {
+    var errorMessages = {
+        incorrect_number: "The card number is incorrect.",
+        invalid_number: "The card number is not a valid credit card number.",
+        invalid_expiry_month: "The card's expiration month is invalid.",
+        invalid_expiry_year: "The card's expiration year is invalid.",
+        invalid_cvc: "The card's security code is invalid.",
+        expired_card: "The card has expired.",
+        incorrect_cvc: "The card's security code is incorrect.",
+        incorrect_zip: "The card's zip code failed validation.",
+        card_declined: "The card was declined.",
+        missing: "There is no card on a customer that is being charged.",
+        processing_error: "An error occurred while processing the card.",
+        rate_limit: "An error occurred due to requests hitting the API too quickly. Please let us know if you're consistently running into this error."
+    };
+
+    if (response.error && response.error.type == 'card_error') {
         // Show the errors on the form
-        $form.find('.payment-errors').text(response.error.message);
+        $form.find('.payment-errors').text(errorMessages[response.error.code]);
         $form.find('button').prop('disabled', false);
 
         $form.find('#CreditCard_CardNumber').addClass('input-validation-error');
